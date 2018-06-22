9334598cc8a509b226db55d1bc7a26e32c748cbe
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-06-22 15:30:37 +0300

    fix BEM

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -5,5 +5,11 @@ module.exports = {
   blue: "#00bfa5",
   "text-color": "#455a64",
   grey: "#373e42",
-  "light-grey": "#dfdcd5"
+  "light-grey": "#dfdcd5",
+  desktop: '1200px', // HD
+  laptop: '992px', // Laptop 11 dpi
+  tablets: '768'+'px',
+  'smartphones-big': '414px',
+  smartphones: '375px',
+  'smartphones-small': '370px'
 };
