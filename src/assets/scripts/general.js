a0c026ced8bb37fecbf012575059359c4bb64345
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-06-26 17:27:27 +0300

    finish flipper

--- src/assets/scripts/general.js
+++ src/assets/scripts/general.js
@@ -1,7 +1,1 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".hero__nav");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-active");
-  nav.classList.toggle("hero__nav--active");
-});
+import "./modules/humb-menu.js";
