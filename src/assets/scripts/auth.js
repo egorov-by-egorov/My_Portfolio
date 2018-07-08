e335f61c63082a36f3e706c31447710488519fb8
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-07-08 18:51:18 +0300

    error: VM4523 bootloader.bundle.js:1 Uncaught TypeError: Cannot read property 'call' of undefined

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,3 +1,4 @@
-import "./modules/flipper.js";
-import "./modules/parralaxMove.js";
 import "./modules/preloader.js";
+import "./modules/parralaxMove.js";
+import "./modules/flipper.js";
+// import "./modules/auth-login.js";
